40a1f6ca1acb638d4ce5755fc58816b4727814fd
Raine DeVries <362571390@GoTVDSB.ca>
2024-10-01 08:39:38 -0400

    Add project files.

---
+++ Topic 6 assignment/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Topic_6_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Raine DeVries
+            int number;
+            int highNumber, lowNumber;
+            int rightNumber;
+            Random random = new Random();
+            Console.WriteLine("What's the lowest number?");
+            lowNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What's the highest number?");
+            highNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The number are "+lowNumber+ " "+ highNumber );    
+            Console.WriteLine(random.Next(lowNumber, highNumber));
+
+            rightNumber = random.Next(1, 11);
+            Console.WriteLine("Guess a number between 1-10");
+            int.TryParse(Console.ReadLine(), out number);
+            if (number == rightNumber)
+            {
+                Console.WriteLine("Your right!");
+            }
+            else { Console.WriteLine("Nope That's not right"); }
+
+
+            Console.WriteLine("I will roll 2 die, here's the output:");
+            Console.WriteLine(random.Next(1, 7)+random.Next(1, 7));
+
+
+
+        }
+    }
+}
